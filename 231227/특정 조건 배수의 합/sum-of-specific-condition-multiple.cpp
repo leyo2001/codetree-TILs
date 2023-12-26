5eb46a376d4069941a595a285fa552b34aa1eddb
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2023-12-27 00:36:00 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231227/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 231227/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -5,14 +5,14 @@ int main() {
     cin>>a>>b;
 
     int sum =0;
-    int counter =0;
+   
 
     for(int i=a;i<=b;i++)
     {
         if(i%5==0)
         {
             sum+=i;
-            counter++;
+           
         }
     }
 
