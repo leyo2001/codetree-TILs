21ea2dfb51db223251ca6fa24ac1f6e9d13617b9
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2023-12-26 16:29:07 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231226/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int c1=0,c2=0; 
+
+    for(int i=1;i<=10;i++)
+    {
+        int a;
+        cin>>a;
+
+        if(a%3==0)
+        {
+            c1++;
+        }
+
+        if(a%5==0)
+        {
+            c2++;
+        }
+
+        cout<<c1<<" "<<c2<<;
+    }    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
