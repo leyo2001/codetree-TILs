b82f6ed1441f243f86c14644b81fd96ea4f39f59
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2023-12-27 00:25:49 +0900

    Title: 100부터 입력받는 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231227/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    int sum=0;
+    for(int i = a; i<=100;i++)
+    {
+        sum+=i;
+    }
+    cout<<sum;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
