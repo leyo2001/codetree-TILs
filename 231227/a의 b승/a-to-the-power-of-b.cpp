b47f1d7cf0ae6126625f7ce6c601388a983d8b52
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2023-12-27 13:43:22 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    int prod=1;
+    for(int i=1; i<=b;i++)
+    {
+        prod*=a;
+        }
+
+        cout<<prod;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
