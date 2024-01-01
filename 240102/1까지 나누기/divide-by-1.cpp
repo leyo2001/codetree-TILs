4b20aaa21b331827d8e13c093c896c569577afeb
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-02 03:04:46 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/1까지 나누기/divide-by-1.cpp
+++ 240102/1까지 나누기/divide-by-1.cpp
@@ -8,13 +8,14 @@ int main() {
     int counter=0;
     for(int i =1; ; i++)
     {
-        counter++;
-        n/=i;
         if(n<=1)
         {
             cout<<counter;
             break;
         }
+        counter++;
+        n/=i;
+        
     }
     // 여기에 코드를 작성해주세요.
     return 0;
