468889c0436d9f0522c43d52bb0323dc01083daa
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-02 01:40:10 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cin>>n;
+
+    int prod=1;
+
+    for(int i=1; i<=10; i++)
+    {
+        prod*=i;
+
+        if(prod>=n)
+        {
+            cout<<i;
+            break;
+        }
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
