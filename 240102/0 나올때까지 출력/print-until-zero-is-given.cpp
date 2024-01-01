892dafed867f9c2ba1534451884009b1344c2de1
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-02 03:43:50 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240102/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+
+    while(true)
+    {
+        if(n==0)
+        break;
+        
+        cin>>n;
+        cout<<n<<endl;
+
+        
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
