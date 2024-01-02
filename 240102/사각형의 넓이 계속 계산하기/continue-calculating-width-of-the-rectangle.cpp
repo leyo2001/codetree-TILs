f8c40b4fdb64c52969cf97b84f96e2991d7f57a6
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-02 23:18:57 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240102/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespaces std;
+int main() {
+
+    int a,b;
+    char c;
+
+    while(true)
+    {
+        cin>>a>>b>>c;
+
+        if(c=='C')
+        {
+            cout<<a*b;
+            break;
+        }
+
+        cout<<a*b<<endl;
+
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
