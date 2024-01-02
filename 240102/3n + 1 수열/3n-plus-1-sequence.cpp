4a3148a6022b213591c0b0f9c4a02bd73228edbf
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-02 23:29:21 +0900

    Title: 3n + 1 수열, Time: 82ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240102/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    int counter =0;
+
+    
+        cin>>n;
+
+    while(n!=1)
+    {
+        if(n%2==2)
+        {
+            n/=2;
+            counter++;
+        }
+        
+        else
+        {
+            n=3*n+1;
+            counter++;
+        }
+    }
+    cout<<counter;
+
+
+ 
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
