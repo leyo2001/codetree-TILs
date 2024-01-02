530a5366a42d938f61f31175ec0dec1237779f25
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-02 23:49:24 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240102/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int cnt=0;
+
+
+    while(n<1000)
+    {
+        if(n%2==0)
+        {
+            n=3*n+1;
+            cnt++;
+        }
+
+        else
+        {
+            n=2*n+2;
+            cnt++;
+        }
+    }
+
+    cout<<cnt;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
