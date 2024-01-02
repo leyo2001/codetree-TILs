e446f05b579da1b0406f60db5ec40bfa5757c416
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-03 01:17:13 +0900

    Title: 소수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240103/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n; 
+    cin>>n;
+    bool a=false;
+    for(int i=1; i<n; i++)
+    {
+        if(n%i==0)
+        {
+            a=true;
+        }
+        
+    }
+
+    if(a==true)
+    {
+        cout<<"P"<<endl;
+    }
+    else
+    {
+        cout<<"C"<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
