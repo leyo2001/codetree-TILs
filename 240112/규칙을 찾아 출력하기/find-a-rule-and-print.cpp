b2a741b97613cca4252cf0270697bcdb870dfbc1
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-12 17:31:48 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240112/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+
+    cin>>n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(i==1 || j==1 || i==n || j==n)
+            {
+                cout<< "* ";
+            }
+
+            else if(i>=j)
+            {
+                cout<<"  ";
+            }
+
+            else
+            {
+                cout<<"* ";
+            }
+
+        
+        }
+        cout<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
