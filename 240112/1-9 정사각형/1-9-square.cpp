690a917cfe024ee200da10b755523520501948bf
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-12 17:52:56 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+
+    cin>>n;
+
+    int cnt=1;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout<<cnt;
+            cnt++;
+            if(cnt>9)
+            {
+                cnt=1;
+            }
+        }
+        cout<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
