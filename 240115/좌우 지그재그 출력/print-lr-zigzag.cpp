3e6b5212ad31f3791557452e09abc74f05293d9e
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-15 18:40:18 +0900

    Title: 좌우 지그재그 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240115/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cin>>n;
+    int cnt=1;
+
+    for(int i=1; i<=n; i++)
+    {
+        if(i%2==1)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                cout<<cnt;
+                cnt++;
+            }
+            cout<<endl;
+        }
+        else
+        {
+            for(int j=1; j<=n; j++)
+            {
+                cout<<cnt+n;
+                cnt--;
+            }
+            cnt+=n;
+            cout<<endl;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
