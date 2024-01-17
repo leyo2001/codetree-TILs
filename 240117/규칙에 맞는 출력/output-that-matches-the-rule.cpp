267d7b7c3192a05a8e6ea493dfe3ac7820437b72
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-17 19:00:21 +0900

    Title: 규칙에 맞는 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/규칙에 맞는 출력/output-that-matches-the-rule.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cin>>n;
+
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(i>n-j+1)
+            {
+                cout<<"  ";
+            }
+            else
+            {
+                cout<<i+j-1<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
