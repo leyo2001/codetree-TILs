bd696443c0fac535429eb5da3da281a7217e6a14
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-22 18:33:13 +0900

    Title: 일직선 구구단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/일직선 구구단/one-line-multiple.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    for (int i=1; i<=2; i++)
+    {
+        for (int j=1; j<=2; j++)
+        {
+            cout<<i<<" * "<<j<<" = "<<i*j<<endl; 
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
