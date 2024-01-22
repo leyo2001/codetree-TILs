d58b0e62ae5f3b717a26b4d0dad9de67e9c94b0d
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-22 22:10:11 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/소수 구하기/get-prime.cpp
+++ 240122/소수 구하기/get-prime.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    bool isprime = true;
+    
     for(int i=2; i<=n; i++)
     {   
 
-        
+        bool isprime = true;
         for(int j=2; j<i; j++)
         {
             if(i%j==0)
