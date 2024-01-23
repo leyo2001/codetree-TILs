71e9230ddaaf597155f0734f976e93132d9a7030
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-23 17:50:28 +0900

    Title: 원소 10개의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int sum =0, val;
+
+
+    for(int i=1; i<=10; i++)
+    {
+        cin>>val;
+        sum = sum + val;
+        
+    }
+    cout<<sum;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
