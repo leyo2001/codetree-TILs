0aa04a841e995550daf07a490cc2286b71be416a
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-23 17:52:53 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char arr[10];
+    char a;
+
+    for(int i=0; i<10; i++)
+    {
+        cin>>a;
+        arr[i] = a;
+    }
+
+    for(int i=0; i<10; i++)
+    {
+        cout<<arr[9-i]<<" ";
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
