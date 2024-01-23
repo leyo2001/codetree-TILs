2cb8b5976172b1d71ffe8dc01e7aa90c842aa9e5
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-23 18:49:08 +0900

    Title: 짝수들을 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240123/짝수들을 거꾸로 출력/print-even-numbers-upside-down.cpp
+++ 240123/짝수들을 거꾸로 출력/print-even-numbers-upside-down.cpp
@@ -4,7 +4,11 @@ int main() {
     int n;
     cin>> n;
 
-    int arr[n]={};
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = 1;
+    }
 
     for(int i=0; i<n; i++)
     {
@@ -17,7 +21,7 @@ int main() {
     }
     for(int i=0; i<n; i++)
     {
-        if(arr[n-1-i]!=0)
+        if(arr[n-1-i]!=1)
         {
             cout<<arr[n-1-i]<<" ";
         }
