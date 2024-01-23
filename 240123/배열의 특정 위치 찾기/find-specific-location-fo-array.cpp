7b754edef2adc0b1f17d2a1bd2cf01a8c8423fd0
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-23 18:55:56 +0900

    Title: 배열의 특정 위치 찾기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240123/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[10];
+    int sum = 0;
+    int cnt = 0;
+    double average;
+    for(int i =0; i<10; i++)
+    {
+        int n;
+        cin>>n;
+        arr[i] = n;
+    }
+
+    for(int i =1 ; i<9; i+=2)
+    {
+        sum += arr[i];
+        cnt++;
+    }
+    for(int i =2 ; i<9; i+=3;)
+    {
+        sum += arr[i];
+        cnt++;
+    }
+
+    average = double(sum) / cnt;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<sum<<" "<<average;
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
