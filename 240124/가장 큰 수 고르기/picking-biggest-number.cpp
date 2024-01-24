1c18b79197650972d58d71883e09ec6533a61942
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-24 17:55:56 +0900

    Title: 가장 큰 수 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int max;
+
+    for(int i=1; i<=10; i++)
+    {
+        int n;
+        cin>>n;
+        if(i==1)
+        {
+            max = n;
+        }
+        else
+        {
+            max < n;
+            max =n;
+        }
+    }
+
+
+    cout<<max;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
