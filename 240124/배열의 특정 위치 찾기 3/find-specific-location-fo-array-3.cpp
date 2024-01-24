4a5f395a85cfa7e49f366941177d6ea23021a674
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-24 11:14:40 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[100];
+    int sum =0;
+    for(int i=0; i<100; i++)
+    {
+        cin>> arr[i];
+        if(arr[i]==0)
+        {
+            sum = arr[i-1] + arr[i-2] + arr[i-3];
+            break;
+        }
+    }
+    cout<<sum;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
