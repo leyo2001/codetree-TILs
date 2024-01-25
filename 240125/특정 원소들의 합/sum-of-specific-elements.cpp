85e2a92937a429c202ec4d08e972fbaa30d98588
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-25 11:34:09 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int arr[4][4];
+    int sum=0;
+
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            cin>>arr[i][j];
+            if(i>=j)
+            {
+                sum+=arr[i][j];
+            }
+        }
+    }
+    cout<<sum;
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
