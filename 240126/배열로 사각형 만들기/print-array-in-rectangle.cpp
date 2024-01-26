d1422774008a160acd24cbdb494fe0c88c711c48
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-26 16:54:23 +0900

    Title: 배열로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int arr [5][5];
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            if(i==0 || j==0)
+            {
+                arr[i][j] = 1;
+            }
+        }
+    }
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            if(i==0 || j==0)
+            {
+                continue;
+            }
+            else
+            {
+                arr[i][j]= arr[i-1][j] + arr[i][j-1];
+            }
+        }
+    }
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
