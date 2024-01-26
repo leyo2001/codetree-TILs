0c373f3405ad482be10796e7f13a71b6d286299e
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-26 17:48:42 +0900

    Title: 격자 모양 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n,m;
+    cin>>n>>m;
+    int arr[n][n]={};
+
+    for(int i=0; i<m; i++)
+    {
+        int x,y;
+        cin>>x>>y;
+
+        arr[x-1][y-1] = x*y;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
