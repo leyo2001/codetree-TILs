433917d2655f52a3fbb5296aad530a306059d9bf
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-01-28 19:33:12 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240128/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+
+    string a,b;
+
+    cin>>a>>b;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
