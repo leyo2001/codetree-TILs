9aa8aaab748d3edb84a51a74e8849c65072a0aa1
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-05 18:43:03 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string str1;
+    string str2;
+
+    cin>>str1>>str2;
+
+    if(str1.length() > str2.length())
+    {
+        cout<<str1<<" "<<str1.length();
+    }
+    else if(str1.length() < str2.length())
+    {
+        cout<<str2<<" "<<str2.length();
+    }
+    else
+    {
+        cout<<"same";
+    }
+
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
