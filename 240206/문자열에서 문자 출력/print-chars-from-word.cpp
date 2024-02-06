4d1e54e4f46e44d2d57f9f037ad868c2fa1cdcfa
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-06 17:15:15 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string a;
+    cin>>a;
+
+    for(int i=0; i<a.length(); i++)
+    {
+        cout<<a[i]<<endl;
+
+    }
+    
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
