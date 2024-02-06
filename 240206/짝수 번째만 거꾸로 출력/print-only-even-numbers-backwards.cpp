9271ede62d50725eb7c9a285f0026c7c57fe2aec
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-06 17:49:42 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
+++ 240206/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
@@ -7,9 +7,19 @@ int main() {
 
     cin>>a;
 
-    for(int i = a.length()-1; i>=0; i-=2)
+    if(a.length() % 2 ==0)
     {
-        cout<<a[i];
+        for(int i = a.length()-1; i>=0; i-=2)
+        {
+            cout<<a[i];
+        }
+    }
+    else
+    {
+        for(int i = a.length()-2; i>=0; i-=2)
+        {
+            cout<<a[i];
+        }
     }
 
     // 여기에 코드를 작성해주세요.
