70be74fd101cbec4ace4bbffc3bf32bf8f16a3a9
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-06 16:40:33 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string arr[10];
+
+    for(int i=0; i<10; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<10; i+=2)
+    {
+        cout<<arr[i]<<endl;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
