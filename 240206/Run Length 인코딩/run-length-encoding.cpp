d1b37d84e4934daeb91d9509b413d5258bf71c53
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-06 17:39:42 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/Run Length 인코딩/run-length-encoding.cpp
+++ 240206/Run Length 인코딩/run-length-encoding.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int leng(int alphabet_cnt)  //alphabet_cnt가 몇자리 인지 구하는 함수
+    {
+        int d = 10;
+        int d_cnt=1;
+        while(true)
+        {
+            if(alphabet_cnt % d ==alphabet_cnt)
+            {
+                return d_cnt;
+            }
+            else
+            {
+                d*=10;
+                d_cnt++;
+            }
+        }
+    }
+
 int main() {
 
     string a;
     cin>>a;
 
-    
     int alphabet_cnt =1;
     int length_cnt =0;
 
@@ -14,15 +32,17 @@ int main() {
     {
         if(a[i]==a[i+1])
         {
-
+            alphabet_cnt++;
         }
         else
-        {
-            length_cnt +=2;
+        {   
+            length_cnt = length_cnt + 1 + leng(alphabet_cnt);
+            alphabet_cnt=1;
         }
     }
 
     cout<<length_cnt<<endl;
+    alphabet_cnt=1;
 
     for(int i=0; i<a.length(); i++)
     {   
