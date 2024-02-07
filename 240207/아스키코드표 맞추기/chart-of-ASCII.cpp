61f0b5f1350d5316c9cb5411a2244c69bbba5526
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 15:30:47 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int arr[5];
+    
+    for(int i=0; i<5; i++)
+    {
+        cin>>arr[i];
+        cout<<(char)arr[i]<<" ";
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
