c71e727138948d81d4bf1d383d9c1321619ababf
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 10:47:53 +0900

    Title: 문자열 전부 붙이기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240207/문자열 전부 붙이기/paste-all-string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    int n;
+    cin>>n;
+
+    sting total = "";
+
+    string a,b,c;
+
+    cin>>a>>b>>c;
+
+    total = total +a+b+c;
+
+    cout<<total;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
