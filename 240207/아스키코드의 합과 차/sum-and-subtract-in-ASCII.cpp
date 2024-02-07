60e2fd380d0fe3142a27a160c6887a4f752a9685
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 15:29:04 +0900

    Title: 아스키코드의 합과 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+    char a,b;
+    cin>>a>>b;
+
+    cout<<(int)a+b<<" "<<abs((int)a-b);
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
