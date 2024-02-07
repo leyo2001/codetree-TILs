2235a2ab33b1eea5df24eb5e7f4e94e50b6c2b69
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 10:46:44 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string a;
+    cin>>a;
+
+    a+="Hello";
+
+    cout<<a;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
