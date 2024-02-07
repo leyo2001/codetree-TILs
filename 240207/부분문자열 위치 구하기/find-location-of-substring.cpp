dd09db5aaad183efe6daf01168a52ace4501b022
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 11:27:35 +0900

    Title: 부분문자열 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/부분문자열 위치 구하기/find-location-of-substring.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string a,b;
+    cin>>a>>b;
+
+    if(a.find(b) != string::npos)
+    {
+        cout<<a.find(b);
+    }
+
+    else
+    {
+        cout<<-1;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
