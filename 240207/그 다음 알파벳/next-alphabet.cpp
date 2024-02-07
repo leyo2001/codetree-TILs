7351372508217e428da2a9a4b30d7e117d7a8af0
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 15:33:50 +0900

    Title: 그 다음 알파벳, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240207/그 다음 알파벳/next-alphabet.cpp
+++ 240207/그 다음 알파벳/next-alphabet.cpp
@@ -4,7 +4,15 @@ int main() {
 
     char a;
     cin>>a;
-    cout<<char(int(a) + 1);
+    
+    if(a == 'z')
+    {
+        cout<<'a';
+    }
+    else
+    {
+        cout<<char(int(a) + 1);
+    }
 
     // 여기에 코드를 작성해주세요.
     return 0;
