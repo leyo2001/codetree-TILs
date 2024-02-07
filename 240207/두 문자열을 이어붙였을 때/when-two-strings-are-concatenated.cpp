b8e7f6e1b0553a480c6523600e2c0eb9a8cce105
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-02-07 10:59:34 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240207/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string a,b;
+
+    if(a+b == b+a)
+    {
+        cout<<"true";
+    }
+    else
+    {
+        cout<<"false";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
