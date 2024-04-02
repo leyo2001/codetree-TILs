96c6bfa80d3add8e762dfe8c6c04d2345529a8db
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-04-02 17:39:51 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240402/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string a,b;
+    cin>>a>>b;
+
+    int cnt =0;
+
+    for(int i=a.length()-1; i>0; i--)
+    {
+        cnt++;
+        string c = "";
+
+        for(int j = i; j<a.length(); j++)
+        {
+            c += a[j];
+        }
+
+        for(int j =0; j<i; j++)
+        {
+            c += a[j];
+        }
+
+        if(c==b)
+        {
+            cout<<cnt;
+            break;
+        }
+    }
+
+    if(cnt == a.length())
+    {
+        cout<<-1;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
