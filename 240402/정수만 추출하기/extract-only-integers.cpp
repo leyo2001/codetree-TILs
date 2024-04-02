78c38ba08be75b88aa45a51cb98d6d69555a2851
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-04-02 17:04:21 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string a,b;
+    cin>>a>>b;
+
+    
+    string ax, bx = "";
+
+    for(int i=0; i<a.length(); i++)
+    {
+        if(a[i]>=48 && a[i]<=57)
+        {
+           ax+= a[i];
+        }   
+        else
+        {
+            break;
+        }
+    }
+    
+    for(int i=0; i<b.length(); i++)
+    {
+        if(b[i]>=48 && b[i]<=57)
+        {
+            bx+= b[i];
+        }   
+        else
+        {
+            break;
+        }
+    }
+    cout<<stoi(ax) + stoi(bx);
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
