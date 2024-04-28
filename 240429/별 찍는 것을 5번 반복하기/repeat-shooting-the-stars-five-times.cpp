495740882ddc87b4bf65af825e5ead41b28eed04
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-04-29 00:24:55 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void print()
+{
+    for(int i=0; i<10; i++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+int main() {
+
+    for(int i=0; i<5; i++)
+    {
+        print();
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
