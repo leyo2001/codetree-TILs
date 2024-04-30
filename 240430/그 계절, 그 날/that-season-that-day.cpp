26db13b0b414db7bb58f56b0ed09cdfa5f0f1078
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-04-30 19:03:20 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+const int month31[7] = {1,3,5,7,8,10,12};
+
+void season(int M)
+{
+    if(M<=2 || M==12)
+    {
+        cout<<"Winter";
+    }
+    else if(M>2 && M<6)
+    {
+        cout<<"Sprint";
+    }
+    else if(M>6 && M<9)
+    {
+        cout<<"Fall";
+    }
+    else
+    {
+        cout<<"Summer";
+    }
+}
+
+bool isYoon(int Y)
+{
+    if((Y%4==0&&Y%100==0&&Y%400==0) ||(Y%4==0&&Y%100!=0))
+        return true;
+    else if(Y%4==0)
+        return true;
+    else
+        return false;
+    
+}
+
+void result(int Y, int M, int D)
+{
+    for(int i=0; i<7; i++)
+    {
+        if(M==month31[i])
+        {
+            season(M);
+            return;
+        }
+    }
+
+    if(M==2)
+    {
+        if(isYoon(Y))
+        {
+            if(D<30)
+            {
+                season(M);
+                return;
+            }
+            else
+            {
+                cout<<-1;
+                return;
+            }
+        }
+        else
+        {
+            if(D<29)
+            {
+                season(M);
+                return;
+            }
+            else
+            {
+                cout<<-1;
+                return;
+            }
+        }
+    }
+    else
+    {   
+        if(D<31)
+        {
+            season(M);
+            return;
+        }
+        else
+        {
+            cout<<-1;
+            return;
+        }
+    }
+}
+
+int main() {
+
+    int Y,M,D;
+    cin>>Y>>M>>D;
+    result(Y,M,D);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
