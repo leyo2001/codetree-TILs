acf4956efecfb75724bcdabb5710379f1559dc20
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-04-30 17:51:55 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+bool isYoon(int y)
+{
+    if(y%100==0 && y%400!=0)
+        return false;
+    if(y%4==0)
+        return true;
+    
+}
+
+
+int main() {
+
+    int y;
+    cin>>y;
+    cout<<boolalpha;
+    cout<<isYoon(y);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
