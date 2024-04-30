9d2c0d81c0f82d4260e5e82d69078212b345193b
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-04-30 18:49:35 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+const int arr1[7] ={1,3,5,7,8,10,12};
+
+
+bool exist(int M, int D)
+{   
+
+    for(int i=0; i<7; i++)
+    {
+        if(arr1[i]==M)
+        {
+            if(D<=31)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    if(M==2)
+    {
+        if(D<=28)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    else
+    {
+        if(D<=30)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+}
+
+
+int main() {
+
+    int M,D;
+    cin>>M>>D;
+    if(exist(M,D))
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
