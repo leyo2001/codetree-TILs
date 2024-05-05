d410f833dfe9090468c19a0b8c5a669cdacb5d15
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-05-06 01:46:52 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    if(n==0)
+        return;
+
+    for(int i=0; i<n; i++)
+        cout<<"* ";
+    cout<<endl;
+    print(n-1);
+    for(int i=0; i<n; i++)
+        cout<<"* ";
+    cout<<endl;
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+    print(n);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
