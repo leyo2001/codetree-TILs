79be39f3d5c03d2e6c4c4d77f036090145ecccd0
leyo2001 <154868562+leyo2001@users.noreply.github.com>
2024-05-12 17:48:41 +0900

    Title: 오름 내림차순 정렬, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240512/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<algorithm>
+#include<functional>
+using namespace std;
+
+bool com(int a, int b)
+{
+    return a>b;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[5];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+5);
+    for(int i=0; i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    sort(arr,arr+5,com);
+
+    for(int i=0; i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
